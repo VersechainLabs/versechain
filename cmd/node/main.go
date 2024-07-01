3518999f29516fcf09a7a0355581cc8eb99400c2
6v <linvan6v@gmail.com>
2024-07-01 15:35:38 +0800

    generate random wallet for test (#6)
    Implement CreateRandomWallet method to generate multiple random public/private key pairs and allocate 100 ETH to each
    
    Implement GenerateTransferEthRequest method for transferring specified ETH amounts from a private key to a designated public key

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -15,7 +15,14 @@ func main() {
 	cairoCfg := config.LoadCairoCfg("./conf/cairo_cfg.toml")
 	gethCfg := evm.LoadEvmConfig("./conf/evm_cfg.toml")
 
-	// go TestSendTransaction(gethCfg, true)
+	//// 1. Generate random wallet with 100 eth
+	//go func() {
+	//	CreateRandomWallet(gethCfg, 10)
+	//}()
+
+	//2. Transfer eth to another addr
+	//requestBody := GenerateTransferEthRequest(gethCfg, "337746e3ff5cd4833088439cd5b695a11cdd185818ddd1c8cf5135c95d643125", "0x8fE1407582B7FA3B76611875a044Cc16533aFeb1", 1)
+	//fmt.Printf("---- Transfer Eth Request Body ----\n%s\n---------\n", requestBody)
 
 	app.StartUpChain(poaCfg, cairoCfg, gethCfg)
 }
