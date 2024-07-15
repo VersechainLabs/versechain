37e281bff9211ef0a215309e35be19c01198d860
liuwei <linvan6v@gmail.com>
2024-07-15 17:30:49 +0800

    implement eth_feeHistory

--- evm/ethrpc/api_backend.go
+++ evm/ethrpc/api_backend.go
@@ -45,10 +45,8 @@ func (e *EthAPIBackend) SyncProgress() ethereum.SyncProgress {
 //	panic("implement me")
 //}
 
-func (e *EthAPIBackend) FeeHistory(ctx context.Context, blockCount uint64, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*big.Int, [][]*big.Int, []*big.Int, []float64, []*big.Int, []float64, error) {
-	//TODO implement me
-	panic("implement me")
-}
+// Move to ethrpc/gasprice.go
+//func (e *EthAPIBackend) FeeHistory(ctx context.Context, blockCount uint64, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*big.Int, [][]*big.Int, []*big.Int, []float64, []*big.Int, []float64, error) {}
 
 func (e *EthAPIBackend) BlobBaseFee(ctx context.Context) *big.Int {
 	//TODO implement me
