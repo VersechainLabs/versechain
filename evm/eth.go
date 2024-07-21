e57af8469b4498b749b8ac3162b7b6c3fe8ed032
liuwei <linvan6v@gmail.com>
2024-07-21 18:06:18 +0800

    fix: SendTx save a wrong tx hash in db

--- evm/eth.go
+++ evm/eth.go
@@ -4,6 +4,9 @@ import (
 	// "github.com/yu-org/yu/common/yerror"
 
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/yu-org/yu/common/yerror"
@@ -270,6 +273,25 @@ func NewSolidity(gethConfig *GethConfig) *Solidity {
 	)
 
 	return solidity
+}
+
+func (s *Solidity) CheckTxn(txn *yu_types.SignedTxn) error {
+	var txReq TxRequest
+	param := txn.GetParams()
+	err := json.Unmarshal([]byte(param), &txReq)
+	if err != nil {
+		return err
+	}
+
+	var txnHash [yu_common.HashLen]byte
+	if len(txReq.Hash.Bytes()) == yu_common.HashLen {
+		copy(txnHash[:], txReq.Hash.Bytes())
+		txn.TxnHash = txnHash
+	} else {
+		return errors.New(fmt.Sprintf("Expected hash to be 32 bytes long, but got %d bytes", len(txReq.Hash.Bytes())))
+	}
+
+	return nil
 }
 
 // ExecuteTxn executes the code using the input as call data during the execution.
