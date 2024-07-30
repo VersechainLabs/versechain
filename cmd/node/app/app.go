b177d664b00b5168ce57fbda3a75d076f9abddd6
6v <linvan6v@gmail.com>
2024-07-30 12:32:27 +0800

    update yu (#32)

--- cmd/node/app/app.go
+++ cmd/node/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	yuconfig "github.com/yu-org/yu/config"
 	"itachi/cairo"
 	"itachi/cairo/config"
 	"itachi/cairo/l1"
@@ -15,10 +16,10 @@ import (
 	"github.com/yu-org/yu/core/startup"
 )
 
-func StartUpChain(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig) {
+func StartUpChain(kernelCfg *yuconfig.KernelConf, poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig) {
 	figure.NewColorFigure("Itachi", "big", "green", false).Print()
 
-	chain := InitItachi(poaCfg, crCfg, evmCfg)
+	chain := InitItachi(kernelCfg, poaCfg, crCfg, evmCfg)
 
 	// Starknet RPC server
 	rpcSrv := starknetrpc.StartUpStarknetRPC(chain, crCfg)
@@ -33,12 +34,12 @@ func StartUpChain(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethC
 
 }
 
-func InitItachi(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig) *kernel.Kernel {
+func InitItachi(kernelCfg *yuconfig.KernelConf, poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig) *kernel.Kernel {
 	poaTri := poa.NewPoa(poaCfg)
 	cairoTri := cairo.NewCairo(crCfg)
 	solidityTri := evm.NewSolidity(evmCfg)
 	chain := startup.InitDefaultKernel(
-		poaTri, cairoTri, solidityTri,
+		kernelCfg, poaTri, cairoTri, solidityTri,
 	)
 	return chain
 }
