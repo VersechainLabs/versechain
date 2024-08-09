c70140992e9e4d945bf41df0130cee1ddeacc977
Frost <froststarbook@gmail.com>
2024-08-09 11:39:24 +0800

    update InitItachi

--- cmd/node/app/app.go
+++ cmd/node/app/app.go
@@ -11,16 +11,18 @@ import (
 	mev_less_poa "itachi/mev-less/poa"
 	"itachi/utils"
 
+	yuconfig "github.com/yu-org/yu/config"
+
 	"github.com/common-nighthawk/go-figure"
 	"github.com/yu-org/yu/apps/poa"
 	"github.com/yu-org/yu/core/kernel"
 	"github.com/yu-org/yu/core/startup"
 )
 
-func StartUpChain(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig, mevLessPoaCfg *mev_less_poa.PoaConfig, mevLessCfg *mev_less.Config) {
+func StartUpChain(kernelCfg *yuconfig.KernelConf, poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig, mevLessPoaCfg *mev_less_poa.PoaConfig, mevLessCfg *mev_less.Config) {
 	figure.NewColorFigure("Itachi", "big", "green", false).Print()
 
-	chain := InitItachi(poaCfg, crCfg, evmCfg, mevLessPoaCfg, mevLessCfg)
+	chain := InitItachi(kernelCfg, poaCfg, crCfg, evmCfg, mevLessPoaCfg, mevLessCfg)
 
 	// Starknet RPC server
 	rpcSrv := starknetrpc.StartUpStarknetRPC(chain, crCfg)
@@ -35,7 +37,7 @@ func StartUpChain(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethC
 
 }
 
-func InitItachi(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig, mevLessPoaCfg *mev_less_poa.PoaConfig, mevLessCfg *mev_less.Config) *kernel.Kernel {
+func InitItachi(kernelCfg *yuconfig.KernelConf, poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethConfig, mevLessPoaCfg *mev_less_poa.PoaConfig, mevLessCfg *mev_less.Config) *kernel.Kernel {
 	// poaTri := poa.NewPoa(poaCfg)
 	cairoTri := cairo.NewCairo(crCfg)
 	solidityTri := evm.NewSolidity(evmCfg)
@@ -47,7 +49,7 @@ func InitItachi(poaCfg *poa.PoaConfig, crCfg *config.Config, evmCfg *evm.GethCon
 	}
 
 	chain := startup.InitDefaultKernel(
-		mevLessPoaTri, mevLessTri, cairoTri, solidityTri,
+		kernelCfg, mevLessPoaTri, mevLessTri, cairoTri, solidityTri,
 	)
 	return chain
 }
--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"flag"
 	"itachi/cairo/config"
 	"itachi/cmd/node/app"
 	"itachi/evm"
+	"os"
 
 	"github.com/yu-org/yu/apps/poa"
+	yuconfig "github.com/yu-org/yu/config"
 	mev_less_config "itachi/mev-less"
 	mev_less_poa "itachi/mev-less/poa"
 
 	"github.com/yu-org/yu/core/startup"
 )
+
+var isDebug bool
+
+func init() {
+	debugFlag := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+	if debugFlag != nil && *debugFlag {
+		isDebug = true
+	}
+
+	if isDebug {
+		_ = os.RemoveAll("./verse_db")
+		_ = os.RemoveAll("./yu")
+		_ = os.RemoveAll("./cairo_db")
+	}
+}
 
 func main() {
 	startup.InitDefaultKernelConfig()
@@ -19,6 +38,14 @@ func main() {
 	gethCfg := evm.LoadEvmConfig("./conf/evm_cfg.toml")
 	mevLessPoaCfg := mev_less_poa.DefaultCfg(0)
 	mevLessCfg := mev_less_config.DefaultCfg()
+
+	var kernelCfg *yuconfig.KernelConf
+
+	if isDebug {
+		kernelCfg = startup.InitKernelConfigFromPath("./conf/yu_debug_cfg.toml")
+	} else {
+		kernelCfg = startup.InitDefaultKernelConfig()
+	}
 
 	//// 1. Generate random wallet with 100 eth
 	//go func() {
@@ -29,5 +56,5 @@ func main() {
 	//requestBody := GenerateTransferEthRequest(gethCfg, "337746e3ff5cd4833088439cd5b695a11cdd185818ddd1c8cf5135c95d643125", "0x8fE1407582B7FA3B76611875a044Cc16533aFeb1", 1)
 	//fmt.Printf("---- Transfer Eth Request Body ----\n%s\n---------\n", requestBody)
 
-	app.StartUpChain(poaCfg, cairoCfg, gethCfg, mevLessPoaCfg, mevLessCfg)
+	app.StartUpChain(kernelCfg, poaCfg, cairoCfg, gethCfg, mevLessPoaCfg, mevLessCfg)
 }
