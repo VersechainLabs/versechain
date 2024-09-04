9549ded7e3a47dc927402316964229969575a90a
liuwei <linvan6v@gmail.com>
2024-09-04 16:36:27 +0800

    impl: cache for usdt price api

--- evm/party_service.go
+++ evm/party_service.go
@@ -6,9 +6,26 @@ import (
 	"fmt"
 	"math/big"
 	"net/http"
+	"sync"
+	"time"
 )
 
+type priceCache struct {
+	price     *big.Int
+	lastFetch time.Time
+	mu        sync.Mutex
+}
+
+var cache = &priceCache{}
+
 func GetUSDTPricePerGasUnit() (*big.Int, error) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	if cache.price != nil && time.Since(cache.lastFetch) < 1*time.Hour {
+		return cache.price, nil
+	}
+
 	// Get the price of USDT through an API
 	resp, err := http.Get("https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=eth")
 	if err != nil {
@@ -28,6 +45,9 @@ func GetUSDTPricePerGasUnit() (*big.Int, error) {
 
 	priceBigInt := new(big.Int)
 	priceBigInt.SetString(fmt.Sprintf("%.0f", priceInETH*1e18), 10)
+
+	cache.price = priceBigInt
+	cache.lastFetch = time.Now()
 
 	return priceBigInt, nil
 }
